92dc576483c5dda1ef15f5dd156808bd9d759fed
Chris Weyl <rsrchboy@cpan.org>
2012-11-25 09:43:01 +0000

    Import of RSRCHBOY/Dist-Zilla-Plugin-ContributorsFromGit-0.001 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-ContributorsFromGit
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         RSRCHBOY/Dist-Zilla-Plugin-ContributorsFromGit-0.001.tar.gz
    gitpan-cpan-author:       RSRCHBOY
    gitpan-cpan-maturity:     released

---
+++ lib/Dist/Zilla/Plugin/ContributorsFromGit.pm
@@ -0,0 +1,138 @@
+#
+# This file is part of Dist-Zilla-Plugin-ContributorsFromGit
+#
+# This software is Copyright (c) 2012 by Chris Weyl.
+#
+# This is free software, licensed under:
+#
+#   The GNU Lesser General Public License, Version 2.1, February 1999
+#
+package Dist::Zilla::Plugin::ContributorsFromGit;
+{
+  $Dist::Zilla::Plugin::ContributorsFromGit::VERSION = '0.001';
+}
+
+# ABSTRACT: Populate your 'CONTRIBUTORS' POD from the list of git authors
+
+use Moose;
+use namespace::autoclean;
+use autobox::Core;
+use List::AllUtils qw{ apply max uniq };
+use Syntax::Keyword::Junction 'none';
+
+use autodie 'system';
+use IPC::System::Simple ( ); # explict dep for autodie system
+
+use aliased 'Dist::Zilla::Stash::PodWeaver';
+
+with
+    'Dist::Zilla::Role::BeforeBuild',
+    'Dist::Zilla::Role::RegisterStash',
+    ;
+
+# debugging...
+#use Smart::Comments '###';
+
+sub before_build {
+    my $self = shift @_;
+
+    ### get our stash, config...
+    my $stash   = $self->zilla->stash_named('%PodWeaver');
+    do { $stash = PodWeaver->new; $self->_register_stash('%PodWeaver', $stash) }
+        unless defined $stash;
+    my $config  = $stash->_config;
+    my @authors = $self->zilla->authors->flatten;
+
+    ### and get our list from git, filtering: "@authors"
+    my @contributors = uniq sort
+        grep  { none(@authors) eq $_ }
+        apply { chomp                }
+        `git log --format="%aN <%aE>"`
+        ;
+
+    my $i = 0;
+    do { $config->{"Contributors.contributors[$i]"} = $_; $i++ }
+        for @contributors;
+
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=for :stopwords Chris Weyl zilla
+
+=head1 NAME
+
+Dist::Zilla::Plugin::ContributorsFromGit - Populate your 'CONTRIBUTORS' POD from the list of git authors
+
+=head1 VERSION
+
+This document describes version 0.001 of Dist::Zilla::Plugin::ContributorsFromGit - released November 25, 2012 as part of Dist-Zilla-Plugin-ContributorsFromGit.
+
+=head1 SYNOPSIS
+
+    ; in your dist.ini
+    [ContributorsFromGit]
+
+    ; in your weaver.ini
+    [Contributors]
+
+=head1 DESCRIPTION
+
+This plugin makes it easy to acknowledge the contributions of others by
+populating a L<%PodWeaver|Dist::Zilla::Stash::PodWeaver> stash with the unique
+list of all git commit authors reachable from the current HEAD.
+
+=head1 OVERVIEW
+
+On collecting the unique list of reachable commit authors from git, we search
+and remove any git authors from the list of authors L<Dist::Zilla> knows
+about.  We then look for a stash named C<%PodWeaver>; if we don't find one
+then we create an instance of L<Dist::Zilla::Stash::PodWeaver> and register it
+with our zilla instance.  Then we add the list of contributors (the filtered
+git authors list) to the stash in such a way that
+L<Pod::Weaver::Section::Contributors> can find them.
+
+Note that you do not need to have the C<%PodWeaver> stash created; it will be
+added if it is not found.  However, your L<Pod::Weaver> config (aka
+c<weaver.ini>) must include the
+L<Contributors|Pod::Weaver::Section::Contributors> section plugin.
+
+=for Pod::Coverage before_build
+
+=head1 SEE ALSO
+
+Please see those modules/websites for more information related to this module.
+
+=over 4
+
+=item *
+
+L<Pod::Weaver::Section::Contributors>
+
+=item *
+
+L<Dist::Zilla::Stash::PodWeaver>
+
+=back
+
+=head1 AUTHOR
+
+Chris Weyl <cweyl@alumni.drew.edu>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2012 by Chris Weyl.
+
+This is free software, licensed under:
+
+  The GNU Lesser General Public License, Version 2.1, February 1999
+
+=cut
